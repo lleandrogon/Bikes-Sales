4b374901651226365367ecf23b57e02cda478169
lleandrogon2004@gmail.com <lleandrogon2004@gmail.com>
2026-08-24 22:49:31 +0000

    feat: Trata os dados de producttexts

--- silver/producttexts.py
+++ silver/producttexts.py
@@ -1,1 +1,30 @@
 # Databricks notebook source
+# /// script
+# [tool.databricks.environment]
+# environment_version = "5"
+# ///
+from pyspark.sql.functions import *
+
+# COMMAND ----------
+
+df = spark.table("bikesales.bronze.producttexts")
+
+# COMMAND ----------
+
+display(df)
+
+# COMMAND ----------
+
+# DBTITLE 1,Análise LONG_DESCR
+df.groupBy("PRODUCTID") \
+    .count() \
+    .filter(col("count") > 1) \
+    .display()
+
+# COMMAND ----------
+
+# DBTITLE 1,Descobrindo chave primária
+df.write.mode("overwrite") \
+    .format("delta") \
+    .option("mergeSchema", "true") \
+    .saveAsTable("bikesales.silver.producttexts")
